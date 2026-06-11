22d4f04a847e2421debefdaab75d311763981479
jalpp <jalp.panchal44@gmail.com>
2026-06-10 20:39:01 -0400

    chore: clean up older local themes cal

--- src/mcp/registerAgine.ts
+++ src/mcp/registerAgine.ts
@@ -1,8 +1,6 @@
 import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
 import { registerLichessTools } from "./lichessToolRegister.js";
 import { registerRenderingTools } from "./renderToolRegister.js";
-import { registerBoardStateTools } from "./boardstateToolRegister.js";
-import { registerThemeAnalysisTools } from "./themeToolRegister.js";
 import { registerUtilsTools } from "./utilToolRegister.js";
 import { registerLocalStockfishTools } from "./stockfishServerRegister.js";
 import { registerCBM } from "./cbmToolRegister.js";
@@ -14,10 +12,8 @@ import { registerPosiraTools } from "./posiraOpeningRegister.js";
 export function registerAgine(server: McpServer): void {
     registerLichessTools(server);
     registerRenderingTools(server);
-    registerBoardStateTools(server);
     registerCBM(server);
     registerLocalStockfishTools(server);
-    registerThemeAnalysisTools(server);
     registerUtilsTools(server);
     registerChessDBTools(server);
     registerNeuralNetTools(server);
